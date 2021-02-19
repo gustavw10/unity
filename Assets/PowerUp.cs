04bd310dc5bc75b74ac7c56d4b4358ab7066d685
Gustav <cph-gw30@cphbusiness.dk>
2021-02-19 11:02:09 +0100

    attempt

---
+++ Assets/PowerUp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+
+    public float multi = 1.4f;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Pickup(other);
+        }
+    }
+
+    void Pickup(Collider player)
+    {
+        Debug.Log("PICKED UP");
+
+        player.transform.localScale *= multi;
+
+        Destroy(gameObject);
+    }
+
+}
